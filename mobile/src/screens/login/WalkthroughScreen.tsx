1fe60249c005d747f1cb64f0eced3ef569ee7c67
MilosByteOut <milos.ciganovic@byteout.com>
2020-07-27 21:57:33 +0200

    responsive changed

--- mobile/src/screens/login/WalkthroughScreen.tsx
+++ mobile/src/screens/login/WalkthroughScreen.tsx
@@ -11,6 +11,7 @@ import { translate } from '../../translations/translate';
 import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 import { dataRealmStore } from '../../stores';
 import Orientation from 'react-native-orientation-locker';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export interface WalkthroughScreenParams {
     /**
@@ -124,25 +125,25 @@ export class WalkthroughScreen extends React.Component<Props, State> {
         const screenParams = this.props.navigation.state.params!;
 
         return (
-            <WalkthroughBackground type={screenParams.step}>
-                {/* STEP 0 */}
+                <WalkthroughBackground type={screenParams.step}>
+                    {/* STEP 0 */}
                     {screenParams.step === 0 ? (
                         <Fragment>
                             <StatusBar barStyle="dark-content" />
 
-                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: 'white', textAlign: 'center' }}>
+                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: 'white', textAlign: 'center', fontSize: scale(23) }}>
                                 {translate('walkthroughWelcome')}
-                        </Typography>
+                            </Typography>
 
-                            <Typography type={TypographyType.bodyRegular} style={{ color: 'white', textAlign: 'center' }}>
+                            <Typography type={TypographyType.bodyRegular} style={{ color: 'white', textAlign: 'center', fontSize: scale(15) }}>
                                 {translate('walkthroughtParagraph')}
-                        </Typography>
+                            </Typography>
 
                             <View style={{ flex: 1, maxHeight: 30 }} />
 
-                            <Typography type={TypographyType.bodyRegular} style={{ color: 'white', textAlign: 'center' }}>
+                            <Typography type={TypographyType.bodyRegular} style={{ color: 'white', textAlign: 'center', fontSize: scale(15) }}>
                                 {translate('walkthroughtParagraph2')}
-                        </Typography>
+                            </Typography>
 
                             <View style={{ flex: 1 }} />
                         </Fragment>
@@ -150,30 +151,30 @@ export class WalkthroughScreen extends React.Component<Props, State> {
 
                     {/* STEP 1 */}
                     {screenParams.step === 1 ? (
-                        <View style={{flex: 0}}>
+                        <View style={{ flex: 0 }}>
                             <StatusBar barStyle="dark-content" />
 
-                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: '#262628', textAlign: 'center',  fontSize: scale(25) }}>
-                            {translate('walkthroughtScreen1Header')}
+                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: '#262628', textAlign: 'center', fontSize: scale(23) }}>
+                                {translate('walkthroughtScreen1Header')}
 
-                        </Typography>
+                            </Typography>
 
-                            <Typography type={TypographyType.bodyRegular} style={{ color: '#262628', textAlign: 'center',  fontSize: scale(15) }}>
-                            {translate('walkthroughtScreen1Paragraph')}
-                        </Typography>
+                            <Typography type={TypographyType.bodyRegular} style={{ color: '#262628', textAlign: 'center', fontSize: scale(15) }}>
+                                {translate('walkthroughtScreen1Paragraph')}
+                            </Typography>
 
                             <View style={{ flex: 1, maxHeight: 30 }} />
 
-                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center',  }}>
+                            <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', }}>
                                 <Switch
                                     value={this.state.followGrowth}
                                     color={themes.getCurrentTheme().theme.variables?.colors?.switchColor}
                                     onValueChange={() => { this.onFollowGrowthChange() }}
                                     style={{ marginRight: 20 }}
                                 />
-                                <Typography type={TypographyType.bodyRegular} style={{ flex: 1, color: '#262628', textAlign: 'left',  fontSize: scale(15) }}>
+                                <Typography type={TypographyType.bodyRegular} style={{ flex: 1, color: '#262628', textAlign: 'left', fontSize: scale(15) }}>
                                     {translate('walkthroughtScreen1FolowGrowth')}
-                            </Typography>
+                                </Typography>
                             </View>
 
                             <View style={{ flex: 1 }} />
@@ -185,12 +186,12 @@ export class WalkthroughScreen extends React.Component<Props, State> {
                         <Fragment>
                             <StatusBar barStyle="dark-content" />
 
-                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: 'white', textAlign: 'center', fontSize: scale(25)}}>
+                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: 'white', textAlign: 'center', fontSize: scale(23) }}>
                                 {translate('walkthroughtScreen2Title')}
-                        </Typography>
+                            </Typography>
 
-                            <Typography type={TypographyType.bodyRegular} style={{ color: 'white', textAlign: 'center',fontSize: scale(15) }}>
-                        </Typography>
+                            <Typography type={TypographyType.bodyRegular} style={{ color: 'white', textAlign: 'center', fontSize: scale(15) }}>
+                            </Typography>
 
                             <View style={{ flex: 1, maxHeight: 30 }} />
 
@@ -208,17 +209,17 @@ export class WalkthroughScreen extends React.Component<Props, State> {
                         <Fragment>
                             <StatusBar barStyle="dark-content" />
 
-                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: '#262628', textAlign: 'center', fontSize: scale(25) }}>
+                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: '#262628', textAlign: 'center', fontSize: scale(23) }}>
                                 {translate('walkthroughtScreen3Title')}
-                        </Typography>
+                            </Typography>
 
                             <Typography type={TypographyType.bodyRegular} style={{ color: '#262628', textAlign: 'center', fontSize: scale(15), }}>
                                 {translate('walkthroughtScreen3Paragraph')}
-                        </Typography>
+                            </Typography>
 
                             <View style={{ flex: 1, maxHeight: 30 }} />
 
-                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center',  }}>
+                            <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', }}>
                                 <Switch
                                     value={this.state.followDevelopment}
                                     color={themes.getCurrentTheme().theme.variables?.colors?.switchColor}
@@ -227,7 +228,7 @@ export class WalkthroughScreen extends React.Component<Props, State> {
                                 />
                                 <Typography type={TypographyType.bodyRegular} style={{ flex: 1, color: '#262628', textAlign: 'left', fontSize: scale(15), }}>
                                     {translate('walkthroughtScreen3FolowDevelopment')}
-                            </Typography>
+                                </Typography>
                             </View>
 
                             <View style={{ flex: 1 }} />
@@ -239,17 +240,17 @@ export class WalkthroughScreen extends React.Component<Props, State> {
                         <Fragment>
                             <StatusBar barStyle="dark-content" />
 
-                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: '#262628', textAlign: 'center', fontSize: scale(25) }}>
+                            <Typography type={TypographyType.headingPrimary} style={{ width: scale(300), color: '#262628', textAlign: 'center', fontSize: scale(23) }}>
                                 {translate('walkthroughtScreen4Title')}
-                        </Typography>
+                            </Typography>
 
                             <Typography type={TypographyType.bodyRegular} style={{ color: '#262628', textAlign: 'center', fontSize: scale(15) }}>
                                 {translate('walkthroughtScreen4Paragraph')}
-                        </Typography>
+                            </Typography>
 
                             <View style={{ flex: 1, maxHeight: 30 }} />
 
-                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', }}>
+                            <View style={{ marginTop: 15, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', }}>
                                 <Switch
                                     value={this.state.followDoctorVisits}
                                     color={themes.getCurrentTheme().theme.variables?.colors?.switchColor}
@@ -258,28 +259,28 @@ export class WalkthroughScreen extends React.Component<Props, State> {
                                 />
                                 <Typography type={TypographyType.bodyRegular} style={{ flex: 1, color: '#262628', textAlign: 'left', fontSize: scale(15) }}>
                                     {/* Želim da evidentiram posete doktoru i da me aplikacija podseća da unosim podatke */}
-                                {translate('walkthroughtScreen4DoctorVisit')}
-                            </Typography>
+                                    {translate('walkthroughtScreen4DoctorVisit')}
+                                </Typography>
                             </View>
 
                             <View style={{ flex: 1 }} />
                         </Fragment>
                     ) : null}
-                <View style={{flex: 0,  marginBottom: scale(20), marginTop: scale(5), flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                    {screenParams.step !== 0 ? (
-                        <TextButton style={{ width: 80 }} color={screenParams.step === 2 ? TextButtonColor.white : TextButtonColor.purple} onPress={() => { this.onBackClick() }}>
-                            {translate('buttonBack')}
-                        </TextButton>
-                    ) : null}
+                    <View style={{ flex: 0, marginTop: scale(25), flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                        {screenParams.step !== 0 ? (
+                            <TextButton style={{ width: 80 }} color={screenParams.step === 2 ? TextButtonColor.white : TextButtonColor.purple} onPress={() => { this.onBackClick() }}>
+                                {translate('buttonBack')}
+                            </TextButton>
+                        ) : null}
 
-                    <RoundedButton
-                        text={translate('buttonNext') + ' >'}
-                        type={RoundedButtonType.hollowPurple}
-                        onPress={() => { this.onNextClick() }}
-                        style={{ width: 150 }}
-                    />
-                </View>
-            </WalkthroughBackground>
+                        <RoundedButton
+                            text={translate('buttonNext') + ' >'}
+                            type={RoundedButtonType.hollowPurple}
+                            onPress={() => { this.onNextClick() }}
+                            style={{ width: 150 }}
+                        />
+                    </View>
+                </WalkthroughBackground>
         );
     }
 
