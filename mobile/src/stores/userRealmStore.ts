1033ef72a584bae9e384fdb27c25394313d6c28f
MilosByteOut <milos.ciganovic@byteout.com>
2020-08-06 11:25:12 +0200

    TS errors fiexd && ChildName error fiexd

--- mobile/src/stores/userRealmStore.ts
+++ mobile/src/stores/userRealmStore.ts
@@ -266,8 +266,11 @@ class UserRealmStore {
         return child?.gender
     }
 
-    public getAllChildren(context: UserRealmContextValue): Child[]{
-        let allChildren = context.realm?.objects<ChildEntity>(ChildEntitySchema.name).map(child => child);
+    public getAllChildren(context?: UserRealmContextValue): Child[]{
+        let allChildren = context ? 
+            context.realm?.objects<ChildEntity>(ChildEntitySchema.name).map(child => child) : 
+            userRealmStore.realm?.objects<ChildEntity>(ChildEntitySchema.name).map(child => child);
+            
         let currentChild = this.getCurrentChild()?.uuid;
 
         let allChildrenList: Child[] = [];
