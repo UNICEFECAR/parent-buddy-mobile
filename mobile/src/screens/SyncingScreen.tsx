81ab8d734fc4153a0cdeb6c8298e0783f595b06b
MilosByteOut <milos.ciganovic@byteout.com>
2020-09-14 14:56:59 +0200

    load data slider installed

--- mobile/src/screens/SyncingScreen.tsx
+++ mobile/src/screens/SyncingScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, ViewStyle, View } from 'react-native';
+import { SafeAreaView, StyleSheet, ViewStyle, View, Dimensions } from 'react-native';
 import { NavigationStackProp, NavigationStackState } from 'react-navigation-stack';
 import { Typography } from '../components';
 import { TypographyType } from '../components/Typography';
@@ -9,6 +9,9 @@ import { translate } from '../translations';
 import { ActivityIndicator } from 'react-native-paper';
 import { scale } from 'react-native-size-matters';
 import FastImage from 'react-native-fast-image';
+// @ts-ignore
+import { SliderBox } from "react-native-image-slider-box";
+import { ceil } from 'lodash';
 
 export interface Props {
     navigation: NavigationStackProp<NavigationStackState>;
@@ -26,20 +29,62 @@ export class SyncingScreen extends React.Component<Props, object> {
             const syncResponse = await syncData.sync();
             const canAppBeOpened = utils.canAppBeOpened();
 
-            setTimeout(() => {utils.gotoNextScreenOnAppOpen()}, 0);
+            setTimeout(() => { utils.gotoNextScreenOnAppOpen() }, 0);
             setTimeout(() => {
                 if (syncResponse instanceof Error && !canAppBeOpened) {
                     throw syncResponse;
                 }
             }, 1000);
         } else {
-            setTimeout(() => {utils.gotoNextScreenOnAppOpen()}, 0);
+            setTimeout(() => { utils.gotoNextScreenOnAppOpen() }, 0);
         }
     }
 
+    private sliderImages() {
+        return <FastImage
+            source={require('../themes/assets/sync_data.png')}
+            style={{ flex: 1, width: '100%' }}
+            resizeMode="cover"
+        />
+    }
+
+
     public render() {
+        let images = [require('../themes/assets/sync_data.png'), require('../themes/assets/sync_data.png'), require('../themes/assets/sync_data.png')]
+        let fastImages = <FastImage
+            source={require('../themes/assets/sync_data.png')}
+            style={{ flex: 1, width: '100%' }}
+            resizeMode="cover"
+        />
         return (
             <SafeAreaView style={styles.container}>
+                <View style={{ flex: 0, marginTop: scale(20) }}>
+                    <Typography type={TypographyType.headingPrimary} style={{ marginBottom: 0, textAlign: 'center' }}>
+                        Halo beba 
+                    </Typography>
+                    <Typography type={TypographyType.headingSecondary}>
+                        Saputnik u roditeljstvu!
+                    </Typography>
+                </View>
+
+                <FastImage
+                    source={require('../themes/assets/sync_data.png')}
+                    style={{ flex: 6, height: 300, width: '100%' }}
+                    resizeMode="cover"
+                />
+                <View style={{ flex: 2}}>
+                    <SliderBox
+                        ImageComponent={FastImage}
+                        images={images}
+                        autoplayInterval={100}
+                        sliderBoxHeight={100}
+                        autoplay={20}
+                        resizeMethod={'resize'}
+                        resizeMode={'cover'}
+                        circleLoop
+                        parentWidth={Dimensions.get('window').width}
+                    />
+                </View>
                 <View style={{ flex: 0, marginTop: scale(20) }}>
                     <Typography type={TypographyType.headingPrimary} style={{ marginBottom: 0 }}>
                         {translate('syncScreenText')}
@@ -48,16 +93,15 @@ export class SyncingScreen extends React.Component<Props, object> {
                         {translate('syncScreenSubText')}
                     </Typography>
                 </View>
-
-                <FastImage
-                    source={ require('../themes/assets/sync_data.png') }
-                    style={{ flex:1, width:'100%' }}
-                    resizeMode="cover"
-                />
-
                 <View style={{ flex: 0, marginBottom: scale(20) }}>
                     <ActivityIndicator size="large" />
                 </View>
+                {/* <SliderBox
+                    images={this.sliderImages()}
+                    sliderBoxHeight={200}
+                   
+                    parentWidth={300}
+                /> */}
             </SafeAreaView>
         );
     }
